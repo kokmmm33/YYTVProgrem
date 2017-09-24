a80e113d1f6f2fa9c2cd748d57c0a48b73e0e99c
蔡杰 <caijie@caijiedemacbook-pro.local>
2017-09-24 12:40:01 +0800

    【研发】初始化项目

---
+++ YYTV/YYTV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YYTV
+//
+//  Created by 蔡杰 on 2017/9/24.
+//  Copyright © 2017年 Joseph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
